c34b749ef7a2acf12f8a2a5cbae8f33f7cab7b20
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2023-02-12 00:09:27 +0530

    sequilize ficx

--- models/booking.js
+++ models/booking.js
@@ -1,4 +1,4 @@
-const {Sequelize,DataTypes,Model}=require('Sequelize');
+const {Sequelize,DataTypes,Model}=require('sequelize');
 const sequelize=require('../config/database');
 
 class Booking extends Model{
