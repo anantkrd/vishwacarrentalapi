e384e83444c60147c33894f0621ea26bde80374f
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2023-02-17 00:05:45 +0530

    auth

--- api/booking/booking.router.js
+++ api/booking/booking.router.js
@@ -4,20 +4,20 @@ const jwt = require('jsonwebtoken');
 const { createUser, getUserByMobile } = require('../user/user.controller');
 const { create_booking, getCabs, create_booking_log, updateCab } = require('../booking/booking.controller');
 const { getSurge, addPayment, updateBookingDetails } = require('../booking/booking.service');
-const authenticate = require("../auth/index");
+
 const router = express.Router();
 var distance = require('google-distance-matrix');
 const Razorpay = require("razorpay");
 const moment = require('moment');
-
+const authenticate = require("../auth/index");
 const bookingController = require("./booking.controller");
 const userController = require("../user/user.controller");
 
-router.post('/book_cab', bookingController.bookCab);
-router.get('/getCabs', bookingController.getCabs);
-router.get('/getBookingById', userController.getBookingById);
-router.post('/payment', bookingController.payment);
-router.post('/success', bookingController.paymentSuccess);
-router.get('/prepayment', bookingController.prepayment);
+router.post('/book_cab',authenticate, bookingController.bookCab);
+router.get('/getCabs',authenticate, bookingController.getCabs);
+router.get('/getBookingById', authenticate,userController.getBookingById);
+router.post('/payment',authenticate, bookingController.payment);
+router.post('/success',authenticate, bookingController.paymentSuccess);
+router.get('/prepayment', authenticate,bookingController.prepayment);
 
 module.exports = router;
