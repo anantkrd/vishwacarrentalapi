17903894edf9f4e94a81b7e40de5a70725049322
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2023-03-12 23:35:26 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const { createPool } = require('mysql');
 const Sequelize=require('sequelize');
-const sequelize=new Sequelize(process.env.MYSQL_DB,process.env.DB_USER,process.env.DB_PASS,{dialect:'mysql',host:process.env.HOST,})
+const sequelize=new Sequelize(process.env.MYSQL_DB,process.env.DB_USER,process.env.DB_PASS,{dialect:'mysql',host:process.env.HOST});
+sequelize.sync();
+
 module.exports=sequelize;
 
 /*
